39488dd4daaf830b95d0456d06d60c932eeff854
pranavasree <pranavareddy27@gmail.com>
2026-07-13 17:19:16 -0400

    Clean up before practising REST API's

--- 03_springBoot_&_spring_data_JPA/example/src/main/java/com/pranava/example/ExampleApplication.java
+++ 03_springBoot_&_spring_data_JPA/example/src/main/java/com/pranava/example/ExampleApplication.java
@@ -4,18 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
 
-		var ctx = SpringApplication.run(ExampleApplication.class, args);
-
-		MyFirstService myFirstService = ctx.getBean("myFirstService",MyFirstService.class);
-
-		System.out.println(myFirstService.tellAStory());
-
-		System.out.println(myFirstService.getMyCustomPropFromAnotherFile());
+		SpringApplication.run(ExampleApplication.class);
 
 	}
 
