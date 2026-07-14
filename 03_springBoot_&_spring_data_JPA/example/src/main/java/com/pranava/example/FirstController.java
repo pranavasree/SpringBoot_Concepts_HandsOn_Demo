74ac4ae033e7a50373a1aa85e479d58655f0904d
pranavasree <pranavareddy27@gmail.com>
2026-07-14 16:37:53 -0400

    JPA concepts

--- 03_springBoot_&_spring_data_JPA/example/src/main/java/com/pranava/example/FirstController.java
+++ 03_springBoot_&_spring_data_JPA/example/src/main/java/com/pranava/example/FirstController.java
@@ -3,6 +3,8 @@ package com.pranava.example;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class FirstController {
 
@@ -18,4 +20,31 @@ public class FirstController {
         return repository.save(student);
 
     }
+
+    @GetMapping("/students")
+    public List<Student> findAllStudents(){
+
+        return repository.findAll();
+    }
+
+    @GetMapping("/students/{student-id}")
+    public Student findStudentById(@PathVariable("student-id") Integer id){
+
+        return repository.findById(id).orElse(new Student());
+
+    }
+
+    @GetMapping("/students/search/{student-name}")
+    public List<Student> findAllByFirstname(@PathVariable("student-name") String name){
+
+
+        return repository.findAllByFirstnameContaining(name);
+    }
+
+    @DeleteMapping("/students/{student-id}")
+    public  void delete(@PathVariable("student-id") Integer id){
+        repository.deleteById(id);
+    }
+
+
 }
--- 03_springBoot_&_spring_data_JPA/example/src/main/java/com/pranava/example/StudentRepository.java
+++ 03_springBoot_&_spring_data_JPA/example/src/main/java/com/pranava/example/StudentRepository.java
@@ -2,7 +2,9 @@ package com.pranava.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
-
+    List<Student> findAllByFirstnameContaining(String firstName);
 }
